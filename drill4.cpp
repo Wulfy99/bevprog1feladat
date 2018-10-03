3ffef1a6413d37847125e527c0ba9fd26d125996
Szabo Krisztian <43440140+Wulfy99@users.noreply.github.com>
2018-10-03 11:23:40 +0200

    Update drill4.cpp

--- drill4.cpp
+++ drill4.cpp
@@ -1,20 +1,19 @@
-#include "std_lib_facilities.h";
-using namespace drill4;
+#include "std_lib_facilities.h"
 
 int main(){
 	double szam; //Ez lesz a az értek ami alapján számolunk
 	string me; //Mértékegység változó rövidítve me
-	vector<<double>> szamok
+	vector < double > szamok;
 
 	while (me != "|"){ 	//Addig fog a cilumenni míg a "|" változót nem kapja meg.Ez a ciklusból való kimenési feltétel.
-		cout << "\nAdjon meg egy mértékegséget(cm, m,  ft, in vagy | -t hogyha be szertné zárni a programot)" // <- bekérjük az me értékét.
+		cout << " \n Adjon meg egy mértékegséget(cm, m,  ft, in vagy | -t hogyha be szertné zárni a programot):" ;// <- bekérjük az me értékét.
 		cin >> me;
-
 		if (me == "|"){
 			return 0;
 		}
 		//else?
-		cout <<"\nAdjon meg egy értéket,"<< me <<  "melyet szerene átváltani méterbe"; //A számot  bekérem.
+		cout <<" \n Adjon meg egy értéket,"<< me <<  "melyet szerene átváltani méterbe"; //A számot  bekérem.
+		cin >> szam;
 		if (me == "cm"){
 			szam= szam/100;
 			szamok.push_back(szam);
@@ -30,16 +29,17 @@ int main(){
 			szam ==  szam/39.37;
 			szamok.push_back(szam);
 		}
-		else cout << (\n"Nem megfelelő formátum/mértékegység") //hibakód, ha egyik iffel sem végbe a művelet.
-			sort(szamok); //Rendezés
-			cout << string(50, '\n'); //esztétikai átrendezés
-			cout << "Az ön értékei: " << a << '\n'; //Adatok kiírása
-			cout << "A legkisebb érték: " << szamok.front() << '\n'; 
-			cout << "a legnagyobb érték: " << szamok.back() << '\n';
-			cout << "Az összes szám: " << szamok.size() << '\n';
-			cout << "A számok növekvő sorrendben: ";
-
+		else{ cout << ("\n Nem megfelelő formátum/mértékegység") ;//hibakód, ha egyik iffel sem végbe a művelet.
+				sort(szamok); //Rendezés
+				cout << string(50, '\n'); //esztétikai átrendezés
+				cout << "Az ön értékei: " << szam << '\n'; //Adatok kiírása
+				cout << "A legkisebb érték: " << szamok.front() << '\n'; 
+				cout << "a legnagyobb érték: " << szamok.back() << '\n';
+				cout << "Az összes szám: " << szamok.size() << '\n';
+				cout << "A számok növekvő sorrendben: ";
+			}
 			for (int i = 0; i < szamok.size(); i++) {
-			cout << szamok[i] << ", "; //Eddigi számok kiírása növekvő sorrendben, egymás után
+				cout << szamok[i] << ", "; //Eddigi számok kiírása növekvő sorrendben, egymás után
+			}
 	}
 }
